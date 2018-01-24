6bbe654e39cc77e968d57e454428491601212fa2
Kamil Golec <kumalgfilms@gmail.com>
2018-01-24 17:29:32 +0100

    AppCenter

--- Testownik/App.xaml.cs
+++ Testownik/App.xaml.cs
@@ -1,22 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
 
 namespace Testownik
 {
@@ -33,6 +25,7 @@ namespace Testownik
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            AppCenter.Start("07b9dcb3-38f6-4413-9587-961e01b296df", typeof(Analytics));
         }
 
         /// Extend acrylic into the title bar. 
